0234bc09050c71a60e2d0991f7e22f0930c299e4
waruiiko <87284108+waruiiko@users.noreply.github.com>
2022-06-16 22:39:16 +0800

    220616

--- page/src/main.js
+++ page/src/main.js
@@ -4,16 +4,32 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-// import http from './http'
+import http from './http'
 const app = createApp(App);
 
-let http =()=>{
-    console.log('http://127.0.0.1:3000')
-    return 'success';
-}
+// let http =()=>{
+//     console.log('http://127.0.0.1:3000')
+//     return 'success';
+// }
 // const http = axios.create({
 //     baseURL: 'http://127.0.0.1:3000',
 // });
-app.config.globalProperties.$http=http;
+// app.config.globalProperties.$http=()=>{
+//     console.log('http://127.0.0.1:3000');
+// }
 // app.config.globalProperties.$http=http;
-app.use(store).use(router).use(VueAxios,axios).mount('#app')
+// app.config.globalProperties.$http=http;
+app.config.globalProperties.$filter = {
+    foo() {
+          return http
+        // return axios.create({
+        //     baseURL: 'http://127.0.0.1:3000',
+        // });
+    },
+    bar() {
+        const baseURL= 'http://127.0.0.1:3000'
+        return baseURL
+    }
+}
+
+app.use(store).use(router).use(VueAxios, axios).mount('#app')
